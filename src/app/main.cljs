2f5549e8ed33167b227ac5b670099284bb936107
Andrzej Fricze <fricze@MacBook-Pro-Andrzej.local>
2019-07-07 10:57:05 +0200

    Tasks

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,34 +1,41 @@
 (ns app.main
   (:require [app.lib :as lib]
+            [app.time :as time]
             [app.most-counter :refer [make-counter-3!]]
             [app.worker :refer [run]]
+            [stylefy.core :as stylefy :refer [use-style]]
             ["react-dom" :refer [render]]
             ["react-hyperscript" :as h]
             ["./calendar.js" :refer [getDaysInMonthRecursive Calendar]]
             [app.calendar-view :refer [calendar-view]]
             [app.get-month :refer [get-days-in-month-loop-3]]))
+(comment
 
-(defn get-calendar-block [props]
-  (clj->js (calendar-view
-            (-> props
-                js->clj
-                clojure.walk/keywordize-keys
-                (clojure.set/rename-keys
-                 {:daysPerRow :days-per-row})))))
+ (defn get-calendar-block [props]
+   (clj->js (calendar-view
+             (-> props
+                 js->clj
+                 clojure.walk/keywordize-keys
+                 (clojure.set/rename-keys
+                  {:daysPerRow :days-per-row})))))
 
-(def root (h Calendar #js {:_getCalendarBlock get-calendar-block}))
+ (def root (h Calendar #js {:_getCalendarBlock get-calendar-block}))
 
-;; (render root (js/document.getElementById "root"))
+ (render root (js/document.getElementById "root"))
 
-(make-counter-3!)
+ (make-counter-3!)
+ )
 
 (defn main! []
-  (run)
+  (time/run)
+
+  ;; maybe move somwhere like 'run'
+  (stylefy/init)
 
   (println "[main]: loading"))
 
 (defn reload! []
 
-  (run)
+  (time/run)
   #_(println "[main] reloaded lib:" lib/c lib/d)
   #_(println "[main] reloaded:" a b))
