e30e6b33ef3cb316b44b30403467ab721ec7bb5a
Andrzej Fricze <fricze@MacBook-Pro-Andrzej.local>
2019-08-11 14:43:59 +0200

    Drag some

--- src/app/dashboard.cljs
+++ src/app/dashboard.cljs
@@ -5,6 +5,7 @@
             [app.dashboard.render :refer [render-component]]
             [app.dashboard.views :as dashboard]
             ["react-useinterval" :as use-interval]
+            ["react" :refer [memo]]
             ["react-pose" :refer [] :default posed]
             ["react-dom" :as react-dom]))
 
@@ -74,37 +75,72 @@
 (def window-height (aget js/window "innerHeight"))
 (def window-width  (aget js/window "innerWidth"))
 
+(defnc GridBox [{:keys [col-height window-width window-height row-height
+                        set-row set-col row col set-diff set-show-col]}]
+  (let [box (.div posed #js {:draggable true
+                             :dragging  #js {:scale 1.2}})]
+    [box
+     {:onValueChange #js {:x (fn [x]
+                               (let [x         (+ x (* (dec col) col-height))
+                                     diff      (/ (- window-width x) col-height)
+                                     new-col   (- 30 (int diff))
+                                     real-diff (js/Math.abs (- (js/Math.floor diff) diff))]
+
+
+                                 (set-diff real-diff)
+                                 (set-show-col (inc new-col))
+
+                                 #_(when (not= new-col show-col)
+                                     (set-show-col (inc new-col)))))}
+      :onDragEnd (fn [e]
+                   (let [row (- 30 (int (/ (- window-height (aget e "y")) row-height)))
+                         col (- 30 (int (/ (- window-width (aget e "x")) col-height)))]
+                     (set-row row)
+                     (set-col col)))
+
+      :style {:grid-row    row
+              :grid-column col
+              :border      "1px solid #333"
+              :cursor      "pointer"}}
+     ""]))
+
+(def grid-box (memo GridBox))
+
 (defnc Root []
   [:div
-   {:style {:display               "grid"
-            :height                "100vh"
-            :width                 "100vw"
-            :grid-template-columns "repeat(30, 1fr)"
-            :grid-template-rows    "repeat(30, 1fr)"}}
+   {}
+   (let [row-height (/ window-height 30)
+         col-height (/ window-width 30)
 
-   #_[:div {:draggable true :ondragstart "event.dataTransfer.setData('text/plain', 'This text may be dragged')"
-            :onDrag    (fn [] (js/console.log :draggy))}
-      [:strong "drag me"]]
+         [show-col set-show-col] (use-state 0)
+         [diff set-diff]         (use-state 0)
 
-   (let [box           (.div posed #js {:draggable true
-                                        :dragging  #js {:scale 1.2}
-                                        :dragEnd   #js {:scale 1}})
          [row set-row] (use-state 1)
          [col set-col] (use-state 1)]
-     [box
-      {:onDragEnd (fn [e]
-                    (let [row-height (/ window-height 30)
-                          col-height (/ window-width 30)
 
-                          row (- 30 (int (/ (- window-height (aget e "y")) row-height)))
-                          col (- 30 (int (/ (- window-width (aget e "x")) col-height)))]
-                      (set-row row)
-                      (set-col col)))
+     [:div
+      {:style {:display               "grid"
+               :height                "100vh"
+               :width                 "100vw"
+               :grid-template-columns "repeat(30, 1fr)"
+               :grid-template-rows    "repeat(30, 1fr)"}}
 
-       :style {:grid-row    row
-               :grid-column col
-               :border      "1px solid #333" :cursor "pointer"}}
-      ""])])
+      [:div {:style {:opacity (if (and
+                                   (< (+ diff 0.5) 1.2)
+                                   (> (+ diff 0.5) 0.7)) 0.7 0) :grid-row "1/30" :grid-column show-col :width 2 :background "rgb(121, 182, 242)"}}]
+      ;; [:div {:style {:opacity 0.7 :grid-row "1/30" :grid-column (dec show-col) :width 2 :background "rgb(121, 182, 242)"}}]
+
+      [grid-box
+       {:col           col
+        :col-height    col-height
+        :window-width  window-width
+        :window-height window-height
+        :row-height    row-height
+        :set-row       set-row
+        :set-col       set-col
+        :row           row
+        :set-diff      set-diff
+        :set-show-col  set-show-col}]])])
 
 (defn run-app! []
   (react-dom/render
