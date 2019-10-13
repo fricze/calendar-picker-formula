a08d5dcb3448a44a627393ce9a11e928785ba0bb
fricze <andrzej.fricze@gmail.com>
2019-10-13 13:53:51 +0200

    Three fn's. Current, previous, next

--- src/app/get_month.cljs
+++ src/app/get_month.cljs
@@ -1,21 +1,12 @@
-(ns app.get-month)
+(ns app.get-month
+  (:require [clojure.spec.alpha :as s]
+            [clojure.spec.test.alpha :as stest]))
 
 (comment
 
   "soooo! you don't have to count from left to right!
 You know which weekday starts and which weekday finishes week… so you can count
 require number of days to right and left")
-
-(defn same-month [date month]
-  (= (.getMonth date) month))
-
-(defn --get-days-in-month [month year]
-  (loop [day  1
-         days []]
-    (let [date (js/Date. year month day)]
-      (if (= (.getMonth date) month)
-        (recur (inc day) (conj days date))
-        days))))
 
 (defn dec-month [{:keys [month year]}]
   (if (= month 0)
@@ -26,6 +17,20 @@ require number of days to right and left")
   (if (= month 11)
     {:month 0 :year (inc year)}
     {:month (inc month) :year year}))
+
+(s/def ::year int?)
+(s/def ::month int?)
+
+(s/def ::date
+  (s/keys :req-un [::month ::year]))
+
+(s/fdef dec-month
+  :args (s/cat :date ::date)
+  :ret ::date)
+
+(s/fdef inc-month
+  :args (s/cat :date ::date)
+  :ret ::date)
 
 (def month->days-no
   [31 28 31 30 31 30
@@ -72,25 +77,18 @@ require number of days to right and left")
     (map #(js/Date. year month %)
          (range first-in-month last-in-month))))
 
+(defn get-days-from-month [{:keys [month year]}]
+  (let [days (range 1 (inc (month->days-no month)))]
+    (map #(js/Date. year month %) days)))
+
 (defn get-days-in-month [{:keys [month year] :as date}]
-  (let [days               (range 1 (inc (month->days-no month)))
-        current-month-days (map #(js/Date. year month %) days)
-
-        prev-month-days (get-days-from-prev-month
-                         date (-> current-month-days first .getDay))
-
-        next-month-days (get-days-from-next-month
-                         date (-> current-month-days last .getDay))]
+  (let [current-month-days (get-days-from-month date)
+        prev-month-days    (get-days-from-prev-month
+                            date (-> current-month-days first .getDay))
+        next-month-days    (get-days-from-next-month
+                            date (-> current-month-days last .getDay))]
 
     (concat prev-month-days current-month-days next-month-days)))
-
-(defn get-days-in-calendar-month [{:keys [month year]}]
-  (loop [day  1
-         days []]
-    (let [date (js/Date. year month day)]
-      (if (= (.getMonth date) month)
-        (recur (inc day) (conj days date))
-        days))))
 
 ;; monday === 1
 ;; sunday === 0
